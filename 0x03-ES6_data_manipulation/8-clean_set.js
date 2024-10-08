8f86cbf00196594816404eff714abbd333455af7
Henry4593 <jonyango4@gmail.com>
2024-10-08 10:35:17 +0300

    fix: fix all format bugs

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(newSet, string) {
-  if (typeof string !== 'string' || !newSet instanceof Set || string.length === 0) {
+  if (typeof string !== 'string' || !(newSet instanceof Set) || string.length === 0) {
     return '';
   }
   const stringLength = string.length;
   let finalString = '';
   newSet.forEach((setItem) => {
-    if (setItem.slice(0,stringLength) === string) {
-      finalString += setItem.slice(stringLength) + '-';
+    if (setItem.slice(0, stringLength) === string) {
+      finalString += `${setItem.slice(stringLength)}-`;
     }
   });
-  return finalString.slice(0,-1);
+  return finalString.slice(0, -1);
 }
