07abaeffd59968e09ea88ed4a31364d0f832e8cd
Henry4593 <jonyango4@gmail.com>
2024-10-08 10:24:11 +0300

    initial commit

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(newSet, string) {
+  if (typeof string !== 'string' || !newSet instanceof Set || string.length === 0) {
+    return '';
+  }
+  const stringLength = string.length;
+  let finalString = '';
+  newSet.forEach((setItem) => {
+    if (setItem.slice(0,stringLength) === string) {
+      finalString += setItem.slice(stringLength) + '-';
+    }
+  });
+  return finalString.slice(0,-1);
+}
